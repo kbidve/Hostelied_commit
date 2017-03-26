f61e0c5b64c535b238a6cceba84fd1c06d220a38
kbidve <bidvekailas@gmail.com>
2017-03-26 15:13:39 +0530

    Using Generic API views

--- PostYourAd/urls.py
+++ PostYourAd/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^cotbasisroom/$', views.CotBasisDetails.as_view(), name='CotBasis-create'),
-    url(r'^cotbasisroom/(?P<ad_id>[0-9]+)/$', views.CotBasisDetails.as_view(), name='CotBasis-create'),
+    url(r'^cotbasisroom/$', views.CotBasisList.as_view(), name='CotBasis-create'),
+    url(r'^cotbasisroom/(?P<pk>[0-9]+)/$', views.CotBasisDetais.as_view(), name='CotBasis-create'),
 ]
