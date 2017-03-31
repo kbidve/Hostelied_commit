194c2242a9b1e4f90232009d952984c2a24994b6
kbidve <bidvekailas@gmail.com>
2017-04-01 00:38:45 +0530

    Added Sub Urls for users ads list, single add , edit add

--- UserAdministrator/urls.py
+++ UserAdministrator/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from . import views
+from PostYourAd.views import CotBasisDetais , FlatOnRentDetais
 
 urlpatterns = [
     url(r'^$', views.UserList.as_view(), name='UserAdministrator-create'),
     url(r'^(?P<pk>[0-9]+)/$', views.UserDetais.as_view(), name='UserAdministrator-list'),
     url(r'^(?P<user_id>[0-9]+)/postedads/$', views.UsersAdsList.as_view(), name='UserAdministrator-postedads'),
+    url(r'^(?P<user_id>[0-9]+)/postedads/rooms$', views.UsersRoomAdsList.as_view(), name='UserAdministrator-postedroomsads'),
+    url(r'^(?P<user_id>[0-9]+)/postedads/flats$', views.UsersFlatAdsList.as_view(), name='UserAdministrator-postedflatsads'),
+    url(r'^(?P<user_id>[0-9]+)/postedads/rooms/(?P<pk>[0-9]+)$', CotBasisDetais.as_view(), name='UserAdministrator-postedroomad'),
+    url(r'^(?P<user_id>[0-9]+)/postedads/flats/(?P<pk>[0-9]+)$', FlatOnRentDetais.as_view(), name='UserAdministrator-postedflatad'),
 ]
--- UserAdministrator/views.py
+++ UserAdministrator/views.py
@@ -2,9 +2,10 @@ from rest_framework import generics
 from .models import UserInfo
 from .serializers import UserSerializer
 from rest_framework.views import APIView
-from PostYourAd.models import CotBasisRooms
-from PostYourAd.serializers import CotBasisRoomsSerializer
+from PostYourAd.models import CotBasisRooms, FlatOnRent
+from PostYourAd.serializers import CotBasisRoomsSerializer, FlatOnRentSerializer
 from rest_framework.response import Response
+from rest_framework.status import  HTTP_201_CREATED
 
 
 class UserList(generics.ListCreateAPIView):
@@ -18,9 +19,52 @@ class UserDetais(generics.RetrieveUpdateDestroyAPIView):
 class UsersAdsList(APIView):
     def get(self, request , user_id ,format='json'):
         queryset= CotBasisRooms.objects.filter(user_id=user_id)
+        querysetflat = FlatOnRent.objects.filter(user_id=user_id)
         serializer= CotBasisRoomsSerializer(queryset , many=True)
-        return Response(serializer.data)
+        serializerflat = FlatOnRentSerializer(querysetflat ,many=True)
+        return Response({
+            'rooms' : serializer.data ,
+            'flats' : serializerflat.data
+            })
 
+
+class UsersRoomAdsList(APIView):
+    def get(self, request , user_id ,format='json'):
+        queryset= CotBasisRooms.objects.filter(user_id=user_id)
+        serializer= CotBasisRoomsSerializer(queryset , many=True)
+        return Response({
+            'rooms' : serializer.data 
+            })
+    def post(self , request, user_id  ,format='json'):
+        serializer = CotBasisRoomsSerializer(data=request.data)
+        if serializer.is_valid():
+            room =serializer.save()
+            if room:
+                return Response(serializer.data, status=HTTP_201_CREATED)
+            
+class UsersFlatAdsList(APIView):
+    def get(self, request , user_id ,format='json'):
+        queryset= FlatOnRent.objects.filter(user_id=user_id)
+        serializer= FlatOnRentSerializer(queryset ,many=True)
+        return Response({
+            'flats' : serializer.data 
+            })
+    
+    def post(self , request, user_id  ,format='json'):
+        serializer = FlatOnRentSerializer(data=request.data)
+        if serializer.is_valid():
+            flat =serializer.save()
+            if flat:
+                return Response(serializer.data, status=HTTP_201_CREATED)
+        
+
+class UsersRoomAdWithId(APIView):
+    def get(self, request , user_id , ad_id ,format='json'):
+        queryset= CotBasisRooms.objects.get(user_id=user_id)
+        serializer= FlatOnRentSerializer(queryset ,many=True)
+        return Response({
+            'rooms' : serializer.data 
+            })
 
 # from rest_framework.views import APIView
 # from rest_framework.response import Response
